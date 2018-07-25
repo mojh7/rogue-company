5e06d69dd033c9b2d69a452f0a90f90cddfeddad
ShootingStarGames <zooin823@naver.com>
2018-07-25 22:09:11 +0900

    Minimap Icon

--- Assets/StageAsset/Scripts/MiniMap.cs
+++ Assets/StageAsset/Scripts/MiniMap.cs
@@ -506,7 +506,7 @@ public class MiniMap : MonoBehaviourSingleton<MiniMap>
     void MoveMinimapIcon()
     {
         transform.localPosition = new Vector2(-(playerPositon.x / mapSizeWidth) * width + width / 2 - maskSize,
-            -(playerPositon.y / mapSizeHeight) * height + height / 2 - maskSize);
+            -(playerPositon.y / mapSizeHeight) * height + height / 2 - maskSize - 0.2f);
     } // 현재 플레이어 위치 to MiniMap
 
     public void ToggleMinimap()
