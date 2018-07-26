0bb702f80cd2aa736616c00bc88d9c8510dac83c
ShootingStarGames <36808020+ShootingStarGames@users.noreply.github.com>
2018-07-26 13:54:18 +0900

    Select UI change

--- Assets/StageAsset/Scripts/MapManager.cs
+++ Assets/StageAsset/Scripts/MapManager.cs
@@ -186,7 +186,7 @@ namespace Map
                     break;
                 }
             }
-            Debug.Log(count);
+
             TileManager.Instance.verticalWallRuleTile.DeleteNull();
             TileManager.Instance.horizonWallRuleTile.DeleteNull();
 
@@ -196,7 +196,6 @@ namespace Map
         {
             for(int i=0;i<settedRoomSet.Count;i++)
             {
-                Debug.Log("A");
                 necessaryRoomSet.Add(settedRoomSet[i]);
             }
             settedRoomSet.Clear();
