211d139aa1c18d735e42924c3fd63884a93bfbe5
ShootingStarGames <36808020+ShootingStarGames@users.noreply.github.com>
2018-07-30 00:07:01 +0900

    폴더 위치 수정 여러가지 수정

--- Assets/ActiveSkillAsset/Scripts/ActiveSkillManager.cs
+++ Assets/ActiveSkillAsset/Scripts/ActiveSkillManager.cs
@@ -36,13 +36,17 @@ public class ActiveSkillManager : MonoBehaviourSingleton<ActiveSkillManager>
         return true;
     }
 
-    public bool HandTrap(Character attacker, Character victim,float time)
+    public bool HandTrap(Character attacker, Character victim, float time, float num)
     {
         if (attacker || victim || time < 0)
         {
             return false;
         }
-        StartCoroutine(CoroutineSkill(HandTrap, attacker, victim, time));
+        for(int i=0;i<num;i++)
+        {
+            float randTime = UnityEngine.Random.Range(0, time + 1);
+            StartCoroutine(CoroutineSkill(HandTrap, attacker, victim, randTime));
+        }
         return true;
     }
 
@@ -65,7 +69,22 @@ public class ActiveSkillManager : MonoBehaviourSingleton<ActiveSkillManager>
 
     private void HandTrap(Character attacker, Character victim)
     {
+        float radius = Vector2.Distance(attacker.transform.position, victim.transform.position);
+        Vector2 randPos = UnityEngine.Random.insideUnitCircle * radius;
+        GameObject gameObject = new GameObject();
+        gameObject.transform.position = new Vector2(attacker.transform.position.x + randPos.x, attacker.transform.position.y + randPos.y);
+        gameObject.AddComponent<Alert>();
+        gameObject.GetComponent<Alert>().Init(HandTrap);
+        gameObject.GetComponent<Alert>().Active();
+    }
 
+    private void HandTrap(Vector3 pos)
+    {
+        GameObject gameObject = new GameObject();
+        gameObject.AddComponent<HandTrap>();
+        gameObject.AddComponent<SpriteRenderer>();
+        gameObject.AddComponent<Animator>();
+        gameObject.transform.position = pos;
     }
 
     #endregion
