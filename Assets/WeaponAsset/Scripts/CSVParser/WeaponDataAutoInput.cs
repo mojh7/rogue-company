d136a1001dba106b09db3dcae8c6104374eb3866
momo <mjh79017901@gmail.com>
2018-07-31 00:00:41 +0900

    웨폰 데이터 자동 입력을 위한 CSV 파싱 준비중

---
+++ Assets/WeaponAsset/Scripts/CSVParser/WeaponDataAutoInput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class WeaponDataAutoInput : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+/*
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Test : MonoBehaviour
+{
+    private void Awake()
+    {
+        List<Dictionary<string, object>> data = CSVParser.Read("456");
+
+        Debug.Log(data.Count);
+        for(var i = 0; i < data.Count; i++)
+        {
+            //name,weaponType,attackAniType,rate
+            Debug.Log(i + " = " + data[i]["name"] + " " + data[i]["weaponType"] + " " + data[i]["attackAniType"] + " " + data[i]["rate"]);
+        }
+    }
+}
+ */
