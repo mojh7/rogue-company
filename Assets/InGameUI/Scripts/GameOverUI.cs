0f10b36dc1e5d194f8d07da4803c3ce25d4177a9
YounaJoo <gkdld299@daum.net>
2018-08-14 16:24:46 +0900

    0814 gameover UI 변경
    기획서대로!
    하지만 이전 층수는 아직 수정해야함

--- Assets/InGameUI/Scripts/GameOverUI.cs
+++ Assets/InGameUI/Scripts/GameOverUI.cs
@@ -8,15 +8,17 @@ public class GameOverUI : MonoBehaviourSingleton<GameOverUI> {
     public Text timeT;
     public Text killT;
     public Text coinT;
-    public Text floorT;
     public Text rewardT;
+    public Image[] floorI;
 
+    [SerializeField] private Sprite[] arrow;
     public GameObject getItems;
     float playTime;
     int kill;
     int coin;
-    int floor;
     int reward;
+    int currentFloor;
+    int preFloor;
 
     // 다들 값을 받아와서 UI에 text형태로 보여줄 값들(형변환 필요)
     #endregion
@@ -27,31 +29,89 @@ public class GameOverUI : MonoBehaviourSingleton<GameOverUI> {
         playTime = GameDataManager.Instance.GetTime();
         kill = GameDataManager.Instance.GetKill();
         coin = GameDataManager.Instance.GetCoin();
-        floor = GameDataManager.Instance.GetFloor();
+        currentFloor = GameDataManager.Instance.GetFloor() + 6;
+        preFloor = 10; // test용
         reward = 0;
     }
     public void LoadSelect()
     {
         GameStateManager.Instance.LoadSelect();
     }
+
     public void LoadTitle()
     {
         GameStateManager.Instance.LoadTitle();
     }
+
     public void LoadData()
     {
         UpdateTimeData(playTime);
-        killT.text = kill.ToString() + " Kill!!";
-        coinT.text = coin.ToString();
-        floorT.text = (5 + floor).ToString() + "F";
+        killT.text ="처치 수        " + kill.ToString();
+        coinT.text = "획득 자금      " + coin.ToString();
+        if (currentFloor != preFloor)
+        {
+            BuildingFloor(currentFloor, false);
+            BuildingFloor(preFloor, true);
+        }
+        else
+        {
+            BuildingFloor(currentFloor, false);
+        }
     }
+
     private void UpdateTimeData(float time)
     {
         string minutes = Mathf.FloorToInt(time / 60).ToString("00");
         string seconds = Mathf.FloorToInt(time % 60).ToString("00");
         string timeStr = minutes + " : " + seconds;
-        timeT.text = "Time : " + timeStr;
+        timeT.text = "플레이 타임    " + timeStr;
     }
+
+    private void BuildingFloor(int floor, bool isPre)
+    {
+        switch (floor)
+        {
+            case 6:
+                floorI[0].gameObject.SetActive(true);
+                if (isPre)
+                    floorI[0].sprite = arrow[1];
+                else
+                    floorI[0].sprite = arrow[0];
+                break;
+            case 7:
+                floorI[1].gameObject.SetActive(true);
+                if (isPre)
+                    floorI[1].sprite = arrow[1];
+                else
+                    floorI[1].sprite = arrow[0];
+                break;
+            case 8:
+                floorI[2].gameObject.SetActive(true);
+                if (isPre)
+                    floorI[2].sprite = arrow[1];
+                else
+                    floorI[2].sprite = arrow[0];
+                break;
+            case 9:
+                floorI[3].gameObject.SetActive(true);
+                if (isPre)
+                    floorI[3].sprite = arrow[1];
+                else
+                    floorI[3].sprite = arrow[0];
+                break;
+            case 10:
+                floorI[4].gameObject.SetActive(true);
+                if (isPre)
+                    floorI[4].sprite = arrow[1];
+                else
+                    floorI[4].sprite = arrow[0];
+                break;
+            default:
+                Debug.Log("오류");
+                break;
+        }
+    }
+
     #endregion
 
     #region unityEngine
