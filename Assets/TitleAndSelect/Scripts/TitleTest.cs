b555e196155db3312d4e9af69524db7aa049299c
YounaJoo <gkdld299@daum.net>
2018-08-21 02:37:56 +0900

    0820-21 타이틀씬 테스트
    테스트 1. 페이드인
    테스트2. 스와이프

---
+++ Assets/TitleAndSelect/Scripts/TitleTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 화면 터치하면 화면 깜빡거리면서 셀렉트이미지로 변경
+
+public class TitleTest : MonoBehaviour {
+
+    [SerializeField] private Text txt;
+    [SerializeField] private Image image;
+    private bool isHide = true;
+
+    [SerializeField] private Image fadeImage;
+
+    public void FadeInScreen()
+    {
+        txt.gameObject.SetActive(false);
+        image.gameObject.SetActive(false);
+        FadeIn(fadeImage, 100);
+    }
+
+    void FadeIn(Image _img, int _interval)
+    {
+        StartCoroutine(CoroutineFadeIn(_img, _interval));
+    }
+
+    IEnumerator CoroutineFadeIn(Image _img, int _interval)
+    {
+        for (int i = _interval; i >= 0; i--)
+        {
+            _img.color = new Color(_img.color.r, _img.color.g, _img.color.b, (float)i / _interval);
+            yield return YieldInstructionCache.WaitForEndOfFrame;
+        }
+        LoadSelect();
+    }
+
+    private void LoadSelect()
+    {
+        GameStateManager.Instance.LoadSelect();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            FadeInScreen();
+        }
+
+        if (isHide)
+        {
+            Color color = txt.color;
+            color.a = color.a - Time.deltaTime;
+
+            if (color.a < 0)
+            {
+                color.a = 0.0f;
+                isHide = false;
+            }
+
+            txt.color = color;
+        }
+        else
+        {
+            Color color = txt.color;
+            color.a = color.a + Time.deltaTime;
+
+            if (color.a > 1)
+            {
+                color.a = 1.0f;
+                isHide = true;
+            }
+            txt.color = color;
+        }
+    }
+}
---
+++ Assets/TitleAndSelect/Scripts/TitleTest2.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleTest2 : MonoBehaviour {
+
+    [SerializeField] private Image arrow;
+    private bool isHide = true;
+
+    [SerializeField] private Image layer;
+
+    [SerializeField] private GameObject[] panel;
+    private bool isSelect = false;
+
+    Vector3 titleDes;
+    Vector3 selectDes;
+
+    float touchStart = 0f;
+    
+    private float swipeSpeed = 0.05f;
+
+    private void Awake()
+    {
+        titleDes = panel[0].transform.position;
+        selectDes = panel[1].transform.position;
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Input.mousePosition.x;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            float delta = Input.mousePosition.x - touchStart;
+            if (delta < -50f)
+            {
+                titleDes = new Vector3(panel[0].transform.position.x - 2f,
+                    panel[0].transform.position.y, panel[0].transform.position.z);
+                selectDes = new Vector3(panel[1].transform.position.x + 2f,
+                    panel[1].transform.position.y, panel[1].transform.position.z);
+            }
+            else if (delta > 50f)
+            {
+                titleDes = new Vector3(panel[0].transform.position.x + 2f,
+                    panel[0].transform.position.y, panel[0].transform.position.z);
+                selectDes = new Vector3(panel[1].transform.position.x - 2f,
+                    panel[1].transform.position.y, panel[1].transform.position.z);
+            }
+        }
+        if (Vector3.Distance(panel[0].transform.position, titleDes) > 0.2f)
+        {
+            if (panel[0].transform.position.x < titleDes.x && panel[1].transform.position.x > selectDes.x)
+            {
+                panel[0].transform.position = new Vector3(panel[0].transform.position.x + swipeSpeed,
+                    panel[0].transform.position.y, panel[0].transform.position.z);
+
+                panel[1].transform.position = new Vector3(panel[1].transform.position.x + swipeSpeed,
+                       panel[1].transform.position.y, panel[1].transform.position.z);
+
+                // layer setactive false
+                layer.gameObject.SetActive(false);
+            }
+            else
+            {
+                panel[0].transform.position = new Vector3(panel[0].transform.position.x - swipeSpeed,
+                       panel[0].transform.position.y, panel[0].transform.position.z);
+
+                panel[1].transform.position = new Vector3(panel[1].transform.position.x - swipeSpeed,
+                       panel[1].transform.position.y, panel[1].transform.position.z);
+
+                // layer setactive true
+                layer.gameObject.SetActive(true);
+            }
+        }
+        if (isHide)
+        {
+            Color color = arrow.color;
+            color.a = color.a - Time.deltaTime;
+
+            if (color.a < 0)
+            {
+                color.a = 0.0f;
+                isHide = false;
+            }
+
+            arrow.color = color;
+        }
+        else
+        {
+            Color color = arrow.color;
+            color.a = color.a + Time.deltaTime;
+
+            if (color.a > 1)
+            {
+                color.a = 1.0f;
+                isHide = true;
+            }
+            arrow.color = color;
+        }
+    }
+}
