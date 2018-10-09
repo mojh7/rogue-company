111334169ebe47297d95a9c0060f74b062e6ee91
YounaJoo <gkdld299@daum.net>
2018-10-09 14:18:41 +0900

    1009 layer On Out
    특강듣는중 흑흑흑

--- Assets/Tutorial/Scripts/TutorialUIManager.cs
+++ Assets/Tutorial/Scripts/TutorialUIManager.cs
@@ -19,4 +19,23 @@ public class TutorialUIManager : MonoBehaviourSingleton<TutorialUIManager>
     {
         layers[i].gameObject.SetActive(act);
     }
+
+    // 클릭 못하게 layer 들여내기
+    public void HoldOn()
+    {
+        for (int i = 0; i < layers.Length; i++)
+        {
+            layers[i].gameObject.SetActive(true);
+        }
+    }
+
+    // 원하는 index 순서까지 layer 뺴기
+    // layer 순서 : 이동 조이스틱 -> 공격 조이스틱 -> 설정창 -> 구르기 -> 무기스와이프
+    public void HoldOut(int index)
+    {
+        for (int i = 0; i < index; i++)
+        {
+            layers[i].gameObject.SetActive(false);
+        }
+    }
 }
