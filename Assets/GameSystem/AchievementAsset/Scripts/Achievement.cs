d6f45ea2af43fb85d97b7ebecd23f55f43c03c64
momo <mjh79017901@gmail.com>
2019-01-05 07:28:21 +0900

    [Dev] Lobby content UI
    SelectScene내에서 할 수 있는 콘텐츠 제작 중(상점, 도감 등)
    일단 UI 작업 중

--- Assets/GameSystem/AchievementAsset/Scripts/Achievement.cs
+++ Assets/GameSystem/AchievementAsset/Scripts/Achievement.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using AchievementAsset;
 
+//TODO : 업적 옵저버 패턴 기반으로 만들 듯?
+// 업적으로만 얻을 수 있는 열리는 캐릭터, 특정 콘텐츠를 보상으로 하거나
+// 공용으로 쓸 수 있는 골드를 주던가, 따로 업적 포인트를 줘서 이걸로만 살 수 있는 콘텐츠를 만들던가 등등
+
 namespace AchievementAsset
 {
     public enum AchievementType { COMMON, MONSTER, WEAPON, ITEM, MONEY }
