b7b24e8745a642475422eeece0863b3231a74714
YounaJoo <gkdld299@daum.net>
2019-01-22 01:12:39 +0900

    파이어베이스테스트

---
+++ Assets/YA_FirebaseTest/GoogleLoginTest.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+
+
+public class GoogleLoginTest : MonoBehaviour {
+
+    [SerializeField] string email;
+    [SerializeField] string password;
+
+    public InputField inputTextEmail;
+    public InputField inputTextPassword;
+    public Text loginResult;
+
+    FirebaseAuth auth;
+
+    void Awake()
+    {
+        // 초기화
+        auth = FirebaseAuth.DefaultInstance;
+        Debug.Log("1234" + auth.GetType());
+    }
+
+
+    // 버튼이 눌리면 실행할 함수.
+    public void JoinBtnOnClick()
+    {
+        email = inputTextEmail.text;
+        password = inputTextPassword.text;
+
+        Debug.Log("email: " + email + ", password: " + password);
+
+        CreateUser();
+    }
+
+    public void LoginBtnOnClick()
+    {
+        email = inputTextEmail.text;
+        password = inputTextPassword.text;
+
+        Debug.Log("email: " + email + ", password: " + password);
+
+        LoginUser();
+    }
+
+    void LoginUser()
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                loginResult.text = "로그인 실패";
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                loginResult.text = "로그인 실패";
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+        });
+    }
+
+
+    void CreateUser(){
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                loginResult.text = "회원가입 실패";
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                loginResult.text = "회원가입 실패";
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            loginResult.text = "회원가입 굿럭";
+        });
+    }
+}
