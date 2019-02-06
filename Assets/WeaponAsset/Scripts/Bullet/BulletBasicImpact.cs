ff0fe50496de18fdc8319ed4d664c02c9333b020
momo <mjh79017901@gmail.com>
2019-02-07 06:48:57 +0900

    [Dev] Apply Bullet Basic Impact in All bulletPresetInfo
    총알 기본 이펙트 색깔별로 (노랑, 초록 ,파랑, 보라, 핑크 추가) 도트 찍고 파티클 만들고 bulletPresetInfo에 일괄 적용!!

--- Assets/WeaponAsset/Scripts/Bullet/BulletBasicImpact.cs
+++ Assets/WeaponAsset/Scripts/Bullet/BulletBasicImpact.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletEffect : MonoBehaviour
+public class BulletBasicImpact : MonoBehaviour
 {
     [SerializeField]
     private float lifeTime;
     [SerializeField]
     private GameObject spriteObj;
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private string colorName;
 
     private void OnEnable()
     {
         spriteObj.SetActive(true);
+        animator.SetTrigger(colorName);
         Invoke("DisableSprite", lifeTime);
     }
 
--- Assets/WeaponAsset/Scripts/InfoGroup/BulletPresets.cs
+++ Assets/WeaponAsset/Scripts/InfoGroup/BulletPresets.cs
@@ -79,33 +79,72 @@ public class BulletPresets : MonoBehaviourSingleton<BulletPresets>
     [ContextMenu("AutoPresetEdit")]
     public void AutoPresetEdit()
     {
-        //for(int i = 0; i < bulletPresetInfoList.Length; i++)
-        //{
-        //    if(0 == bulletPresetInfoList[i].effectId)
-        //    {
-        //        bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactRed;
-        //        Debug.Log(i + " : " + bulletPresetInfoList[i].effectId + ", red");
-        //    }
-        //    else
-        //    {
-        //        Debug.Log(i + " : " + bulletPresetInfoList[i].effectId + ", effectId 없음");
-        //    }
-        //}
+        for (int i = 0; i < bulletPresetInfoList.Length; i++)
+        {
+            string str = bulletPresetInfoList[i].presetType.ToString();
+            if(str.Contains("RED"))
+            {
+                bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactRed;
+                Debug.Log(i + ", " + str + " : RED");
+            }
+            else if (str.Contains("YELLOW"))
+            {
+                bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactYellow;
+                Debug.Log(i + ", " + str + " : YELLOW");
+            }
+            else if (str.Contains("GREEN"))
+            {
+                bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactGreen;
+                Debug.Log(i + ", " + str + " : GREEN");
+            }
+            else if (str.Contains("BLUE"))
+            {
+                bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactBlue;
+                Debug.Log(i + ", " + str + " : BLUE");
+            }
+            else if (str.Contains("PURPLE") || str.Contains("VIOLET"))
+            {
+                bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactPurple;
+                Debug.Log(i + ", " + str + " : VIOLET~");
+            }
+            else if (str.Contains("PINK"))
+            {
+                bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactPink;
+                Debug.Log(i + ", " + str + " : PINK");
+            }
+            else
+            {
+                Debug.Log(i + ", " + str);
+            }
+        }
 
-        //for (int i = 37; i < bulletpresetinfolist.length; i++)
-        //{
-        //    int refindex = 31 + ((i - 37) % 6);
-        //    bulletpresetinfolist[i].scalex = bulletpresetinfolist[refindex].scalex;
-        //    bulletpresetinfolist[i].scaley = bulletpresetinfolist[refindex].scaley;
-        //    bulletpresetinfolist[i].collidertype = bulletpresetinfolist[refindex].collidertype;
-        //    bulletpresetinfolist[i].autosizeratio = bulletpresetinfolist[refindex].autosizeratio;
-        //    bulletpresetinfolist[i].spriteanimation = bulletpresetinfolist[refindex].spriteanimation;
-        //    bulletpresetinfolist[i].lifetime = bulletpresetinfolist[refindex].lifetime;
-        //    bulletpresetinfolist[i].effectid = 0;
-        //    bulletpresetinfolist[i].manualsize = bulletpresetinfolist[refindex].manualsize;
-        //    bulletpresetinfolist[i].collideroffset = bulletpresetinfolist[refindex].collideroffset;
-        //    bulletpresetinfolist[i].circlemanualradius = bulletpresetinfolist[refindex].circlemanualradius;
-        //    bulletpresetinfolist[i].isfixedangle = bulletpresetinfolist[refindex].isfixedangle;
-        //}
+            //for(int i = 0; i < bulletPresetInfoList.Length; i++)
+            //{
+            //    if(0 == bulletPresetInfoList[i].effectId)
+            //    {
+            //        bulletPresetInfoList[i].bulletImpactType = BulletImpactType.BasicImpactRed;
+            //        Debug.Log(i + " : " + bulletPresetInfoList[i].effectId + ", red");
+            //    }
+            //    else
+            //    {
+            //        Debug.Log(i + " : " + bulletPresetInfoList[i].effectId + ", effectId 없음");
+            //    }
+            //}
+
+            //for (int i = 37; i < bulletpresetinfolist.length; i++)
+            //{
+            //    int refindex = 31 + ((i - 37) % 6);
+            //    bulletpresetinfolist[i].scalex = bulletpresetinfolist[refindex].scalex;
+            //    bulletpresetinfolist[i].scaley = bulletpresetinfolist[refindex].scaley;
+            //    bulletpresetinfolist[i].collidertype = bulletpresetinfolist[refindex].collidertype;
+            //    bulletpresetinfolist[i].autosizeratio = bulletpresetinfolist[refindex].autosizeratio;
+            //    bulletpresetinfolist[i].spriteanimation = bulletpresetinfolist[refindex].spriteanimation;
+            //    bulletpresetinfolist[i].lifetime = bulletpresetinfolist[refindex].lifetime;
+            //    bulletpresetinfolist[i].effectid = 0;
+            //    bulletpresetinfolist[i].manualsize = bulletpresetinfolist[refindex].manualsize;
+            //    bulletpresetinfolist[i].collideroffset = bulletpresetinfolist[refindex].collideroffset;
+            //    bulletpresetinfolist[i].circlemanualradius = bulletpresetinfolist[refindex].circlemanualradius;
+            //    bulletpresetinfolist[i].isfixedangle = bulletpresetinfolist[refindex].isfixedangle;
+            //}
     }
 }
