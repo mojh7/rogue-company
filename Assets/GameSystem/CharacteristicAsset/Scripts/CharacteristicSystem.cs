905d5f6ee52963338fbfdf7df0be4bb9a68807b5
momo <mjh79017901@gmail.com>
2019-03-08 21:12:41 +0900

    Add Boss Weapon(최종보스 3_6원탄)

--- Assets/GameSystem/CharacteristicAsset/Scripts/CharacteristicSystem.cs
+++ Assets/GameSystem/CharacteristicAsset/Scripts/CharacteristicSystem.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CharacteristicSystem : MonoBehaviour {
+public class CharacteristicSystem : MonoBehaviourSingleton<CharacteristicSystem>
+{
+    #region variables
+    [SerializeField] private GameObject characteristicUI;
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region get / set
+    #endregion
+
+    #region unityFunc
+    private void Awake()
+    {
+        characteristicUI.SetActive(false);
+    }
+    #endregion
+
+    #region func
+    public void OpenCharacteristicUI()
+    {
+        characteristicUI.SetActive(true);
+        AudioManager.Instance.PlaySound(0, SOUNDTYPE.UI);
+    }
+
+    public void CloseCharacteristicUI()
+    {
+        characteristicUI.SetActive(false);
+        AudioManager.Instance.PlaySound(0, SOUNDTYPE.UI);
+    }
+    #endregion
+
+    #region coroutine
+    #endregion
 }
