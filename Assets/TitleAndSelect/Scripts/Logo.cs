0814387e8be3b6cb4dda9bb06966e88d2cc85292
momo <mjh79017901@gmail.com>
2019-05-10 23:20:39 +0900

    Modify Title Resource
    게임 이름 텍스트 그림, 타이틀 배경화면, 화면을 터치해주세요 변경

--- Assets/TitleAndSelect/Scripts/Logo.cs
+++ Assets/TitleAndSelect/Scripts/Logo.cs
@@ -60,17 +60,21 @@ public class Logo : MonoBehaviourSingleton<Logo>
         if (image != null)
         {
             Vector2 teamLogoSize = new Vector2(600, 0);
+            Vector3 teamLogoRotation = new Vector3(0, 0, 0);
             image.gameObject.SetActive(true);
             for (int i = 0; i < 15; i++)
             {
+                image.color = new Color(1, 1, 1, (float)i / 15);
                 teamLogoRectTransform.sizeDelta = teamLogoSize;
+                teamLogoRectTransform.rotation = Quaternion.Euler(teamLogoRotation);
+                teamLogoRotation.z = -2f + 4f * ((i/2) % 2);
                 teamLogoSize.y += 250f / 15;
                 yield return YieldInstructionCache.WaitForSeconds(0.02f);
             }
             yield return YieldInstructionCache.WaitForSeconds(2f);
             for (int i = 20; i >= 0; i--)
             {
-                image.color = new Color(255, 255, 255, (float)i / 20);
+                image.color = new Color(1, 1, 1, (float)i / 20);
                 yield return YieldInstructionCache.WaitForSeconds(0.05f);
             }
         }
